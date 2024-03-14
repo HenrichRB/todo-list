6fa0e9a0cb09bf5c5ec3c361a6e2a7ac29c68fa2
Henrich Ribeiro <henrichrb@gmail.com>
2024-03-14 18:19:20 -0300

    add the buttons initial scripts

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 window.addEventListener('load', () =>{
-    let $modal = document.querySelector('#modal')
-
     
+    // botão de adicionar
+    document.getElementById('btn-add').addEventListener('click', () => {
+        let $modal = document.getElementById('modal');
+        $modal.classList.toggle('modal-active');
+    })
+    
+    // botão de salvar
+    document.getElementById('btn-salvar').addEventListener('click', (event) => {
+        event.preventDefault();
+        let $listaTarefa = document.getElementById('listaTarefa');
+        let $task = document.getElementById('task');
+        let tarefaAtual = $task.value;
+        $listaTarefa.innerHTML += `<h3>${tarefaAtual}</h3>`
+    })
+
+    // botão de cancelar
+    document.getElementById('btn-cancelar').addEventListener('click', (event) => {
+        event.preventDefault();
+        let $modal = document.getElementById('modal')
+        $modal.classList.remove('modal-active');
+    })
+
 })
